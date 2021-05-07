83293c83ce5e324449f5bf3ece91341ce69b1c86
51967 <51967@192.168.1.5>
2021-05-07 01:36:50 -0500

    Cajero actualizado

--- src/com/senati/eti/Cajero_auto.java
+++ src/com/senati/eti/Cajero_auto.java
@@ -9,7 +9,6 @@ public class Cajero_auto {
 		Scanner entrada = new Scanner(System.in);
 		Clases_cajero cc = new Clases_cajero();
 		
-		
 		String[] arrtarjeta = {"981-189", "125-485", "123-798", "369-852"};
 		String[] arrnombre = { "Victor Marquina", "Angel Flores", "Juan Benavides", "Israel Montes"};
 		String[] arrcontra = { "2478", "7845", "1265", "7891"};
@@ -37,9 +36,13 @@ public class Cajero_auto {
 		System.out.print("Ingrese número de tarjeta: ");
 		n_tarjeta = entrada.nextLine();
 		
-		if (!(n_tarjeta.equals(cc.Codigo(n_tarjeta))))
-			System.out.print("Tarjeta incorrecta......");
-		
+		if (!(n_tarjeta.equals(cc.Codigo(n_tarjeta)))) {
+			System.out.println("Tarjeta incorrecta......");
+			System.out.println("-----------------------------------------------------");
+			System.out.println("<<<<<<<<<< MUCHAS GRACIAS, VUELVA PRONTO >>>>>>>>>>");
+			System.out.println("-----------------------------------------------------");
+			return;
+		}
 		
 		int contador = 0;
 		do {
@@ -48,32 +51,38 @@ public class Cajero_auto {
 		    contra = entrada.nextLine();
 		
 		    if(contra.equals(cc.pass(n_tarjeta))) {
+		    	System.out.println("\nBIENVENIDO A LA PLATAFORMA");
+		    	System.out.println("****************************");
 		    	System.out.print("\nNombre del titular....: " + cc.nom(n_tarjeta));
+		    	System.out.println("\nSaldo disponible: " + cc.saldo_actual(n_tarjeta));
 		    
-		    int bandera = 0;
 			int seleccion = 0;
 			
 				do {
 					System.out.println("\n");
-					System.out.println("\nPor favor seleccione una opción");
-					System.out.println("         1. Consulta de cuenta");
-					System.out.println("         2. Retiro en efectivo");
-					System.out.println("         3. Deposito en efectivo");
-					System.out.println("         4. Salir");
-					System.out.print("\nOpción: " );
+					System.out.println("   \n¿Que operación desea realizar?");
+					System.out.println("    1. Consulta de cuenta");
+					System.out.println("    2. Retiro en efectivo");
+					System.out.println("    3. Deposito en efectivo");
+					System.out.println("    4. Salir");
+					System.out.print("\nSeleccione una opción: " );
 					seleccion = entrada.nextInt();
 					
 					float retiro = 0, deposito = 0;
 					
 					switch(seleccion) {
 					case 1:
-						System.out.print("Titular....: " + cc.nom(n_tarjeta));
+						System.out.println("\n");
+						System.out.println("<<<<<<<CONSULTA DEL SALDO>>>>>>>");
+						System.out.print("\nTitular....: " + cc.nom(n_tarjeta));
 						System.out.println("\nSaldo disponible: " + cc.saldo_actual(n_tarjeta));
 						break;
 					case 2:
-						System.out.print("¿Cuanto desea retirar? : ");
+						System.out.println("\n");
+						System.out.println("<<<<<<<RETIRO>>>>>>>");
+						System.out.print("\n¿Cuanto desea retirar? : ");
 						retiro = entrada.nextFloat();
-						if(retiro < cc.saldo_actual(n_tarjeta)) {
+						if(retiro <= cc.saldo_actual(n_tarjeta)) {
 							System.out.println("Usted a retirado: " + retiro);
 						    System.out.println("Su nuevo saldo es: "+ cc.retiro(n_tarjeta, retiro));
 						}else 
@@ -81,14 +90,18 @@ public class Cajero_auto {
 						break;
 					
 					case 3:
-						System.out.print("¿Cuanto desea depositar? : ");
+						System.out.println("\n");
+						System.out.println("<<<<<<<DEPOSITO>>>>>>>");
+						System.out.print("\n¿Cuanto desea depositar? : ");
 						deposito = entrada.nextFloat();
 							System.out.println("Usted a depositado: " + deposito);
 						    System.out.println("Su nuevo saldo es: "+ cc.deposito(n_tarjeta, deposito));
 						    break;
 					case 4:
-						System.out.println("¡Gracias! vuelva pronto tulon");
-						break;
+						System.out.println("-----------------------------------------------------");
+						System.out.println("<<<<<<<<<< MUCHAS GRACIAS, VUELVA PRONTO >>>>>>>>>>");
+						System.out.println("-----------------------------------------------------");
+						return;
 						}
 					if(seleccion < 1 || seleccion > 4) {
 						
@@ -105,35 +118,28 @@ public class Cajero_auto {
 						System.out.print("\n¿Desea realizar otra operación[S|N]: ");
 					    rpta = entrada.nextLine();
 					    if (!rpta.toUpperCase().equals("S") && !rpta.toUpperCase().equals("N"))
-					    System.out.println("Opción no valida.........");
+					    System.out.println("<<<<<<<<Opción no valida>>>>>>>>");
 					}
 					}while(!rpta.toUpperCase().equals("S") && !rpta.toUpperCase().equals("N"));
 					
 					
 					if ("S".equals(rpta.toUpperCase()))
 						seleccion = 5;
-					else
-						break;
-					
-					if("N".equals(rpta.toUpperCase())) 
+					else if("N".equals(rpta.toUpperCase())) 
 						seleccion = 4;
 					
 				} while (seleccion < 1 || seleccion > 4);
 		
 		    }else {
-             System.out.print("Contraseña no valida.....");
+             System.out.println("Contraseña no valida.....");
+             System.out.println("Quedan " + ((contador - 2)* -1) + " intentos");
+             contador++;
 		    
-		    contador++;
-		
-		    
-		    }}}while(contador > 0 && contador != 3) ;
-		 
-			
-			
-		
-		}
-	}
+		    }}}while(contador > 0 && contador != 3);
 		
 		
-		
-	
+		System.out.println("-----------------------------------------------------");
+		System.out.println("<<<<<<<<<< MUCHAS GRACIAS, VUELVA PRONTO >>>>>>>>>>");
+		System.out.println("-----------------------------------------------------");	
+	  }
+}
